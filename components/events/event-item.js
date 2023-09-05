646aebb4ab59eaca8d0c0fdb72183e59810af920
MichelleMathuloe <124686615+MichelleMathuloe@users.noreply.github.com>
2023-09-05 16:35:46 +0200

    commi

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,16 +1,25 @@
 import Link from 'next/link'
 
 function EventItem(props) {
+    const { title, image, date, location, id } = props;
+
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    });
+    const formattedAddress = location.replace(', ', '\n');
+
     return <li>
         <img src="" alt="" />
         <div>
             <div>
-                <h2>TITLE</h2>
+                <h2>{title}</h2>
                 <div>
-                 <time>DATE</time>
+                 <time>{humanReadableDate}</time>
                 </div>
                 <div>
-                    <address>ADDRESS</address>
+                    <address>{formattedAddress}</address>
                 </div>
             </div>
             <div>
